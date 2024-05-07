c19179f5013c6abc8f08435017d0f4bc000881d4
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-07 13:00:24 +1000

    update example

--- examples/datazone domain simple/outputs.tf
+++ examples/datazone domain simple/outputs.tf
@@ -1,35 +1,44 @@
 output "datazone_role_id" {
   value = module.datazone_domain.datazone_role_id
+  description = "DataZone Role Id"
 }
 
 output "datazone_role_arn" {
   value = module.datazone_domain.datazone_role_arn
+  description = "DataZone Role ARN"
 }
 
 output "datazone_domain_id" {
   value = module.datazone_domain.datazone_domain_id
+  description = "DataZone Domain Id"
 }
 
 output "environment_project_id" {
   value = module.datazone_domain.environment_project_id
+  description = "Environment Project Id"
 }
 
 output "s3_datazone" {
   value = module.datazone_domain.s3_datazone
+  description = "DataZone S3 DataZone Bucket"
 }
 
 output "s3_datazone_region" {
   value = module.datazone_domain.s3_datazone_region
+  description = "DataZone S3 DataZone Bucket Region"
 }
 
 output "datazone_portal" {
   value = module.datazone_domain.datazone_portal
+  description = "DataZone Portal URL"
 }
 
 output "datazone_status" {
   value = module.datazone_domain.datazone_status
+  description = "DataZone Status"
 }
 
 output "awscc_executionrole" {
   value = module.datazone_domain.awscc_executionrole
+  description = "Datazone Execution Role"
 }
