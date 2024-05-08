89fa7223bb483480431b492710bfefcdd97684cf
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-08 12:51:39 +1000

    add blueprint to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,38 +1,47 @@
 ## Place your outputs here for your module
-
-/* output "output-example" {
-  value       = vault_policy.policies
-  description = "Sample helm values file that contains all of the configured paths that were created with this module. This should be used a reference and not a raw input to another object"
-} */
 output "datazone_role_id" {
   value = awscc_iam_role.this.role_id
+  description = "Datazone Execution Role Id"
 }
 
 output "datazone_role_arn" {
   value = awscc_iam_role.this.arn
+  description = "DataZone Execution Role ARN"
 }
 output "datazone_domain_id" {
   value = awscc_datazone_domain.this.domain_id
+  description = "DataZone domain id"
 }
 
 output "projects" {
   value = awscc_datazone_project.this
+  description = "DataZone projects"
 }
 output "s3_datazone" {
   value = aws_s3_bucket.datazone.id
+  description = "name of the datazone s3 bucket"
 }
 output "s3_datazone_region" {
   value = aws_s3_bucket.datazone.region
+  description = "region of the datazone s3 bucket"
+}
+
+output "datazone_environment_blueprints" {
+  value = awscc_datazone_environment_blueprint_configuration.this
+  description = "DataZone environment blueprints"
 }
 
 output "datazone_portal" {
   value = awscc_datazone_domain.this.portal_url
+  description = "DataZone portal URL"
 }
 
 output "datazone_status" {
   value = awscc_datazone_domain.this.status
+  description = "DataZone status"
 }
 
 output "awscc_executionrole" {
   value = awscc_iam_role.this
+  description = "DataZone execution role object"
 }
